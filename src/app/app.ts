d976e319b063750f4e18fd7c7d4b5ff128221c1c
Mahmoud Mo <mmahmoud092002@gmail.com>
2025-10-23 18:57:20 +0300

    edits

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Login } from './features/auth/login/login';
+import { Component, signal } from "@angular/core";
+import { NavigationEnd, Router, RouterOutlet } from "@angular/router";
+import { filter } from "rxjs";
+import { Header } from "./shared/header/header";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+  selector: "app-root",
+  imports: [RouterOutlet, Header, CommonModule],
+  templateUrl: "./app.html",
+  styleUrl: "./app.scss",
 })
 export class App {
-  protected readonly title = signal('iti-community-frontend');
+  protected readonly title = signal("iti-community-frontend");
+
+  isAdminRoute = false;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.isAdminRoute = event.url.includes("/admin");
+      });
+  }
 }
